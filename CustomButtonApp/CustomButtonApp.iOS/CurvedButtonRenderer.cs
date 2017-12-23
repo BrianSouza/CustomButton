874354bad9c0bf00cea8ce99bc7ebe7ff17addea
sumitsisodia.xamarinskills@gmail.com <sumitsisodia@elensoft.com>
2017-12-23 19:02:34 +0530

    Add project files.

---
+++ CustomButtonApp/CustomButtonApp.iOS/CurvedButtonRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+using CustomButtonApp.iOS;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+using CustomButtonApp;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CurvedButtonRenderer))]
+namespace CustomButtonApp.iOS
+{
+    public   class CurvedButtonRenderer: ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+{
+    base.OnElementChanged(e);
+    var view = (CustomButton)Element;
+    if (view == null) return;
+    Paint(view);
+}
+
+protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+{
+    base.OnElementPropertyChanged(sender, e);
+    if (e.PropertyName == CustomButton.CustomBorderRadiusProperty.PropertyName ||
+       e.PropertyName == CustomButton.CustomBorderColorProperty.PropertyName ||
+       e.PropertyName == CustomButton.CustomBorderWidthProperty.PropertyName)
+    {
+        if (Element != null)
+        {
+            Paint((CustomButton)Element);
+        }
+    }
+}
+private void Paint(CustomButton view)
+{
+    this.Layer.CornerRadius = (float)view.CustomBorderRadius;
+    this.Layer.BorderColor = view.CustomBorderColor.ToCGColor();
+    this.Layer.BackgroundColor = view.CustomBackgroundColor.ToCGColor();
+    this.Layer.BorderWidth = (int)view.CustomBorderWidth;
+}
+    }
+}
